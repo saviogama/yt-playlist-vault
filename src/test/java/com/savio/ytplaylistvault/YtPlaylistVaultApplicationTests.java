fdbe6b2bba2f6068e4ffd2ba23f4c82d77c14189
Savio Gama <savio.gama09@gmail.com>
2026-07-05 14:46:55 -0300

    feat: bootstrap spring boot application

---
+++ src/test/java/com/savio/ytplaylistvault/YtPlaylistVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savio.ytplaylistvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtPlaylistVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
